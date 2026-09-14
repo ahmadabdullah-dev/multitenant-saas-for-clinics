55cf3e07237a36b3b0140a8d92ad80af637934ae
Ahmad Abdullah <ahmadabdullah.formal@gmail.com>
2026-09-14 17:42:40 +0300

    feat(API:Infastructure:Common): Add Base IRepository and Base Repository

---
+++ Infrastructure/Interfaces/Common/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Infrastructure.Interfaces.Common;
+
+public interface IRepository<T> where T : class
+{
+    Task<T?> GetByIdAsync(Guid id);
+    Task<List<T>> GetAllAsync();
+    Task AddAsync(T entity);
+    void Update(T entity);
+    void Remove(T entity);
+    Task<int> SaveChangesAsync();
+}   
