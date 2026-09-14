f4320a954d1a9e4cecf059f5ae53860db4270b54
Ahmad Abdullah <ahmadabdullah.formal@gmail.com>
2026-09-14 13:18:34 +0300

    feat(API:Domain:Platform): Add NewsLetterSubscriber Entity

--- Domain/GlobalUsings.cs
+++ Domain/GlobalUsings.cs
@@ -1,3 +1,4 @@
 ﻿global using Domain.Platform;
 global using Domain.Tenant;
 global using Domain.Common;
+global using Domain.Platform.Marketing;
---
+++ Domain/Platform/Marketing/NewsLetterSubscriber.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Platform.Marketing;
+public class NewsLetterSubscriber : BaseEntity
+{
+    public string Email { get; set; } = default!;
+    public bool IsConfirmed { get; set; } = false;
+    public string? ConfirmationCode { get; set; }
+    public DateTime? ConfirmationCodeSentAtUtc { get; set; }
+    public int FailedAttempts { get; set; } 
+    public DateTime SubscribedAtUtc { get; set; } = DateTime.UtcNow;
+    public DateTime? UnsubscribedAtUtc { get; set; }
+}
